24b4521a221a64b7b6761b385fdd0fb6ec341876
r-motta <riccardomotta04@gmail.com>
2024-04-24 22:32:51 +0200

    risolto errori

--- src/Main.java
+++ src/Main.java
@@ -290,13 +290,13 @@ public class Main {
 			
 			//parte grafica per mostarre le carte inziaili per fronte e retro
 			
-			
+		int j=0;
 			
 			//assegnazione delle carte inziali e decisione su fronte e retro
 			for(int i=0;i<numGiocatori;i++)
 			{
 				
-					int j;
+					
 					
 					if(i==0)
 						j=0;
@@ -343,12 +343,12 @@ public class Main {
 			
 			
 			
-			mazzoObiettivo.shuffle();//mischia le carte obbiettivo
+			Collections.shuffle(mazzoObiettivo);//mischia le carte obbiettivo
 			
 			//assegnazione delle carte OBIETTIVO
 			for(int i=0;i<numGiocatori;i++)
 			{
-					int j=0,k=1;
+					int y=0,k=1;
 					
 					
 
@@ -358,19 +358,22 @@ public class Main {
 					int scelta=0;
 					
 					
+					do
+					{
+						System.out.println("scegli una tra le due carte obbiettivo");
+						System.out.println("[1] prima carta");
+						System.out.println("[2] seconda carta");
+						scelta= sc.nextInt();
+					}while(scelta<1 || scelta>2);
 					
-					System.out.println("scegli una tra le due carte obbiettivo");
-					System.out.println("[1] prima carta");
-					System.out.println("[2] seconda carta");
-					scelta= sc.nextInt();
 					
 					switch(scelta)//serve per gestire la scelta delle due carte obbiettivo
 					{
 					case 1:
 					{
 
-						gamers[i].setcartaObiettivoSegreta(mazzoObiettivo.get(j));
-						
+						gamers[i].setCartaObiettivoSegreta(mazzoObiettivo.get(y));
+						mazzoObiettivo.remove(y);
 						
 						break;
 					}
@@ -378,7 +381,7 @@ public class Main {
 					case 2:
 					{
 						
-						gamers[i].setcartaObiettivoSegreta(mazzoObiettivo.get(k));
+						gamers[i].setCartaObiettivoSegreta(mazzoObiettivo.get(k));
 						
 						k++;	
 						
