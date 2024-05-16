5e2d189cb97d70a159c19986432954fdf76b47fe
frass <frass@192.168.1.22>
2024-05-16 18:59:40 +0200

    Inserito controlli punti per CO: 2,3,4,5

--- src/codex/CartaObiettivo.java
+++ src/codex/CartaObiettivo.java
@@ -22,7 +22,7 @@ public class CartaObiettivo{
 	
 	public void controllo(CartaObiettivo co, Giocatore g)
 	{
-		//co1 va fatto controllo solo in basso perché quando si scorre la matrice trova sempre quella in alto a destra per prima
+		//co1 va fatto controllo solo in basso perchï¿½ quando si scorre la matrice trova sempre quella in alto a destra per prima
 		if(co.requisiti.equals("Diagonale dx di tre carte rosse"))
 		{
 			for(int i=0;i<AreaDiGioco.maxRighe; i++)
@@ -37,26 +37,142 @@ public class CartaObiettivo{
 							{
 								if(g.getAreaDiGioco().getArea()[i+2][j-2].getColore().equals(Color.RED))
 								{
-									System.out.println("Diagonale dx di tre carte rosseo completata! ");
+									System.out.println("Diagonale dx di tre carte rosse completata! ");
 									g.incrementaPunti(2);
 									
 								}
 							}
 							else
-								System.out.println("Diagonale dx di tre carte rosseo NON completata! ");
+								System.out.println("Diagonale dx di tre carte rosse NON completata! ");
 						}
 						else
-							System.out.println("Diagonale dx di tre carte rosseo NON completata! ");
+							System.out.println("Diagonale dx di tre carte rosse NON completata! ");
 					}
 					else
-						System.out.println("Diagonale dx di tre carte rosseo NON completata! ");
+						System.out.println("Diagonale dx di tre carte rosse NON completata! ");
 				}
 			}
 		}
 		//seconda carta
-		else if
+		else if (co.requisiti.equals("Diagonale dx di tre carte rosse"))
 		{
-			
+			for(int i=0;i<AreaDiGioco.maxRighe; i++)
+			{
+				for(int j=0; j<AreaDiGioco.maxColonne; j++)
+				{
+					if(g.getAreaDiGioco().getArea()[i][j]!=null)
+					{
+						if(g.getAreaDiGioco().getArea()[i][j].getColore().equals(Color.GREEN))
+						{
+							if(g.getAreaDiGioco().getArea()[i+1][j+1].getColore().equals(Color.GREEN))
+							{
+								if(g.getAreaDiGioco().getArea()[i+2][j+2].getColore().equals(Color.GREEN))
+								{
+									System.out.println("Diagonale sx di tre carte verdi completata! ");
+									g.incrementaPunti(2);
+									
+								}
+							}
+							else
+								System.out.println("Diagonale sx di tre carte verdi NON completata! ");
+						}
+						else
+							System.out.println("Diagonale sx di tre carte verdi NON completata! ");
+					}
+					else
+						System.out.println("Diagonale sx di tre carte verdi NON completata! ");
+				}
+			}
+		}
+		else if (co.requisiti.equals("Diagonale dx di tre carte blu"))
+		{
+			for(int i=0;i<AreaDiGioco.maxRighe; i++)
+			{
+				for(int j=0; j<AreaDiGioco.maxColonne; j++)
+				{
+					if(g.getAreaDiGioco().getArea()[i][j]!=null)
+					{
+						if(g.getAreaDiGioco().getArea()[i][j].getColore().equals(Color.BLUE))
+						{
+							if(g.getAreaDiGioco().getArea()[i+1][j-1].getColore().equals(Color.BLUE))
+							{
+								if(g.getAreaDiGioco().getArea()[i+2][j-2].getColore().equals(Color.BLUE))
+								{
+									System.out.println("Diagonale dx di tre carte blu completata! ");
+									g.incrementaPunti(2);
+									
+								}
+							}
+							else
+								System.out.println("Diagonale dx di tre carte blu NON completata! ");
+						}
+						else
+							System.out.println("Diagonale dx di tre carte blu NON completata! ");
+					}
+					else
+						System.out.println("Diagonale dx di tre carte blu NON completata! ");
+				}
+			}
+		}
+		else if (co.requisiti.equals("Verticale di due carte verdi e diagonale in basso a sx di una carta magenta"))
+		{
+			for(int i=0;i<AreaDiGioco.maxRighe; i++)
+			{
+				for(int j=0; j<AreaDiGioco.maxColonne; j++)
+				{
+					if(g.getAreaDiGioco().getArea()[i][j]!=null)
+					{
+						if(g.getAreaDiGioco().getArea()[i][j].getColore().equals(Color.GREEN))
+						{
+							if(g.getAreaDiGioco().getArea()[i+1][j].getColore().equals(Color.GREEN))
+							{
+								if(g.getAreaDiGioco().getArea()[i+2][j-1].getColore().equals(Color.MAGENTA))
+								{
+									System.out.println("Verticale di due carte verdi e diagonale in basso a sx di una carta magenta completata!");
+									g.incrementaPunti(3);
+									
+								}
+							}
+							else
+								System.out.println("Verticale di due carte verdi e diagonale in basso a sx di una carta magenta NON completata! ");
+						}
+						else
+							System.out.println("Verticale di due carte verdi e diagonale in basso a sx di una carta magenta NON completata! ");
+					}
+					else
+						System.out.println("Verticale di due carte verdi e diagonale in basso a sx di una carta magenta NON completata! ");
+				}
+			}
+		}
+		else if (co.requisiti.equals("Verticale di due carte rosse e diagonale in basso a dx di una carta blu"))
+		{
+			for(int i=0;i<AreaDiGioco.maxRighe; i++)
+			{
+				for(int j=0; j<AreaDiGioco.maxColonne; j++)
+				{
+					if(g.getAreaDiGioco().getArea()[i][j]!=null)
+					{
+						if(g.getAreaDiGioco().getArea()[i][j].getColore().equals(Color.RED))
+						{
+							if(g.getAreaDiGioco().getArea()[i+1][j].getColore().equals(Color.RED))
+							{
+								if(g.getAreaDiGioco().getArea()[i+2][j+1].getColore().equals(Color.BLUE))
+								{
+									System.out.println("Verticale di due carte rosse e diagonale in basso a dx di una carta blu completata!");
+									g.incrementaPunti(3);
+									
+								}
+							}
+							else
+								System.out.println("Verticale di due carte rosse e diagonale in basso a dx di una carta blu NON completata! ");
+						}
+						else
+							System.out.println("Verticale di due carte rosse e diagonale in basso a dx di una carta blu NON completata! ");
+					}
+					else
+						System.out.println("Verticale di due carte rosse e diagonale in basso a dx di una carta blu NON completata! ");
+				}
+			}
 		}
 	}
 	
